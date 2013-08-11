e8658c4e7a82db973a7d42daad985b29f49e746a
Andrew Iannaccone <andrewiemail@gmail.com>
2013-08-11 10:53:28 +0000

    Indenting

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -1,3 +1,6 @@
+/*jslint browser:true */
+/*globals require */
+
 var pageMod = require("sdk/page-mod");
 var self = require("sdk/self");
 
@@ -16,15 +19,21 @@ pageMod.PageMod({
     contentScriptWhen: "ready",
 
     // Run the extension scripts in the Firefox JS context
-    contentScriptFile: [ self.data.url("settings.js"),
-                         self.data.url("autokick.js"),
-                         self.data.url("avatars.js"),
-                         self.data.url("blacklist.js"),
-                         self.data.url("lobby_ratings.js"),
-                         self.data.url("kingdom_generator.js"),
-                         self.data.url("logviewer.js"),
-                         self.data.url("automatchSeekPop.js"),
-                         self.data.url("automatchOfferPop.js"),
-                         self.data.url("automatchGamePop.js"),
-                         self.data.url("automatch.js") ]
+    contentScriptFile: [
+        self.data.url("backbone-min.js"),
+
+        self.data.url("settings.js"),
+
+        self.data.url("autokick.js"),
+        self.data.url("avatars.js"),
+        self.data.url("blacklist.js"),
+        self.data.url("lobby_ratings.js"),
+        self.data.url("kingdom_generator.js"),
+        self.data.url("logviewer.js"),
+
+        self.data.url("automatchSeekPop.js"),
+        self.data.url("automatchOfferPop.js"),
+        self.data.url("automatchGamePop.js"),
+        self.data.url("automatch.js")
+    ]
 });
