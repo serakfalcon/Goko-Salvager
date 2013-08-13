26264cbfbbfb08d055dc67cca1bc1040c35dabc9
Michael Brandt <michael.j.brandt@colorado.edu>
2013-08-13 13:21:58 -0600

    add a class to scripts we add to the page

--- src/dev/runInPageContext.js
+++ src/dev/runInPageContext.js
@@ -7,6 +7,7 @@ wrapper = function () {
 runInPageContext = function (fn) {
 	var script = document.createElement('script');
 	script.textContent = '('+ fn +')();';
+	script.classList.add('goko-salvager');
 	document.body.appendChild(script);
 };
 
