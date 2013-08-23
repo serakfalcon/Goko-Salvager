75dd03c68530f24fb3afe7dc1817d75c7339b4ee
Andrew Iannaccone <andrewiemail@gmail.com>
2013-08-23 08:03:22 +0000

    Correct errors in gsAlsoDo

--- src/ext/utils.js
+++ src/ext/utils.js
@@ -1,20 +1,20 @@
-gsAlsoDo = function (prototype, methodname, fnBefore, fnAfter) {
+gsAlsoDo = function (object, methodname, fnBefore, fnAfter) {
 
     // Cache old version without overriding previous old versions' names
     var methodname_o = '_' + methodname + '_orig';
-    while (typeof prototype[methodname_o] !== 'undefined') {
+    while (typeof object[methodname_o] !== 'undefined') {
         methodname_o = methodname_o + 'X';
     }
-    prototype[methodname_o] = prototype[methodname];
+    object[methodname_o] = object[methodname];
 
     // Run old version sandwiched between "before" and "after" functions
-    prototype[methodname] = function () {
-        if (typeof fnBefore !== 'undefined') {
-            fnBefore(arguments);
+    object[methodname] = function () {
+        if (typeof fnBefore !== 'undefined' && fnBefore !== null) {
+            fnBefore.apply(this, arguments);
         }
-        var out = prototype[methodname_o].apply(prototype, arguments);
-        if (typeof fnAfter !== 'undefined') {
-            fnAfter(arguments);
+        var out = this[methodname_o].apply(this, arguments);
+        if (typeof fnAfter !== 'undefined' && fnAfter !== null) {
+            fnAfter.apply(this, arguments);
         }
         return out;
     };
