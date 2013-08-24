7ae8efcc96dc07715d1021c7995ea8f3ba5810a8
Andrew Iannaccone <andrewiemail@gmail.com>
2013-08-24 09:41:33 +0000

    Finish writing VP Counter toggle logic. Not yet tested.

--- src/ext/vpcounter.js
+++ src/ext/vpcounter.js
@@ -1,16 +1,29 @@
-/*jslint browser:true, devel:true, nomen:true, forin:true, vars:true */
-/*globals $, _ */
+/*jslint browser:true, devel:true, nomen:true, forin:true, vars:true, regexp:true */
+/*globals $, _, gsAlsoDo */
 
-var x = function (cdbc) {
+var x = function (gs, dc, cdbc, mroom) {
     "use strict";
+    var handleChat, handleLog, announceLock, isMyT1, sendChat, getScores, formatForChat,
+        deckVPValue, cardVPValue, pname, cardTypes, sum;
+ 
+    var salvagerURL = ''; // TODO
+    // TODO: UI displays VP counter if (s === true) or ((s === null) && (any(p)))
+    // TODO: synchronize
+    
+    // TODO
+    sendChat = function (message) {};
+    // TODO
+    getScores = function () {};
+    // TODO
+    formatForChat = function (scores) {};
 
-    var cardTypes = {};
+    cardTypes = {};
     cdbc.map(function (card) {
         cardTypes[card.name[0]] = card.type;
     });
 
-    var sum = function (a, b) { return a + b; };
-    var cardVPValue = function (card, cardCounts) {
+    sum = function (a, b) { return a + b; };
+    cardVPValue = function (card, cardCounts) {
         var c, d = cardCounts;
         switch (card) {
         case 'Dame Josephine':
@@ -47,247 +60,139 @@ var x = function (cdbc) {
         }
     };
 
-    var deckVPValue = function (deck) {
+    deckVPValue = function (deck) {
         return deck.map(function (card) {
             return cardVPValue(card, deck);
         }).reduce(sum);
     };
 
     // TODO: do with angular instead (?)
-    var pname, pnames = [];
+    // TODO: sort on update
+    // TODO: add this to the display
     $('<div id="vpdiv"/>').css('position', 'absolute')
                           .css('padding', '2px')
                           .css('background-color', 'gray');
     $('<table id="vptable"/>').appendTo($('vpdiv'));
-    for (pname in pnames) { // TODO
+    for (pname in gs.vp.pnames) {
         var pindex = 0; // TODO
         var row = $('<tr/>').attr('id', pname + 'VPRow')
                             .addClass('p' + pindex);
         $('<td/>').text(pname).appendTo(row);
         $('<td/>').attr('id', pname + 'VP').appendTo(row);
         $('#vptable').append(row);
-        // TODO: sort on update
     }
-};
 
-var loadVPToggleModule = function () {
-    "use strict";
-
-    var botadvGame;
-    var titleToggle = null;
-
-    // Option describes T1 behavior
-
-    //var respondToVPToggleChat = function (message, sender) {
-    //    console.log('Chat from ' + sender + ': ' + message);
-    //    // TODO: reply
-    //}
-
-    // TODO: intercept #vpon and #vpoff chats when (s !== null) <?>
-    // TODO: never lock in games without humans
-    // TODO: UI displays VP counter if (s === true) or ((s === null) && (any(p)))
-    // TODO: how to reliably detect game start?
-
-    // Tests:
-    // 1. Set vp_bot_enable
-    //    a. start bot game, verify ON
-    //       advance to T5
-    //       send #vpoff, verify OFF
-    //       send #vpon, verify ON
-    //    b. repeat 1a for adventure game
-    // 2. Set vp_bot_enable = false
-    //    a. start bot game, verify OFF
-    //       advance to T5
-    //       send #vpon, verify ON
-    //       send #vpoff, verify OFF
-    //    b. repeat 2a for adventure game
-    // 3. Set vp_human_disallow
-    //    a. start multiplayer game w/o title, verify OFF
-    //       send #vpon/#vpoff, verify OFF and no chat sent
-    //       recieve #vpon/#vpoff, verify OFF
-    //       advance to T5, verify OFF
-    //    b. start multiplayer game with #vpoff in title
-    //       send #vpon/#vpoff, verify OFF and no chat sent
-    //       recieve #vpon/#vpoff, verify OFF
-    //       advance to T5, verify OFF
-    //    c. start multiplayer game with #vpon in title
-    //       send #vpon/#vpoff, verify ON and no chat sent
-    //       recieve #vpon/#vpoff, verify ON
-    //       advance to T5, verify ON
-    // 4. Set vp_human_request
-    //    a. start multiplayer game w/o title, verify OFF
-    //       advance to T1, verity #vpon sent
-    //
-    //
-    
-    // Cases:
-    // Bot Mode:
-    // - vp_bot_enable true --> ON
-    // - send #vpon --> ON
-    // - send #vpoff --> OFF
-    // - T5 --> no effect
-    // Adv Mode: (same as bot mode)
-    // Multiplayer Mode:
-    // - title #vpon/#vpoff --> ON/OFF
-    //   - vp_human_X --> no effect
-    //   - send/receive ANY --> no effect
-    // - vp_human_disallow --> OFF, send #vpoff
-    //   - send/receive any --> no effect
-    // - vp_human_request --> ON, send #vpon
-    //   - send/receive #vpoff --> OFF
-    //     - any --> no effect
-    // - T5 lock
-    //   - any --> no effect
-    // * test order doesn't matter
-
-    var updateVPCounterToggle = function (messageName, messageData, message, dc) {
-        var m, tablename = JSON.parse(dc.table.get('settings')).name;
-        var botadvGame = (typeof tablename === 'undefined');
-
-        if (['addLog', 'RoomChat', 'gameSetup'].indexOf(messageName) < 0) {
-            return;
+    // Listen to log and chat messages
+    gsAlsoDo(dc, 'onIncomingMessage', function (messageName, messageData, message) {
+        if (messageName === 'addLog') {
+            handleLog(messageData.text);
+        } else if (messageName === 'RoomChat') {
+            var speaker = mroom.playerList.findByAddress(
+                messageData.playerAddress
+            ).get('playerName');
+            handleChat(speaker, messageData.text);
         }
+    });
 
-        console.log('Message Name: ' + messageName);
-        console.log(messageData.text);
-        console.log(messageData);
+    isMyT1 = function (logText) {
+        var m = logText.match(/(.*): turn 1/);
+        return m !== null
+            && m[1] === mroom.localPlayer.get('playerName')
+            && !gs.vp.locked;
+    };
 
-        // TODO: show stating on half-turn after flip
+    handleLog = function (text) {
 
-        // advbot game, vp_human_request, vp_human_disallow
-        // lastRequest
-        // title has #vpon
-        // title has #vpoff
-        // anyone said #vpoff
-        // anyone said #vpon
-        //
-        // Triggers:
-        // my T1 if vp_human_request, vp_human_disallow, vp_bot_enable, titleOn, titleOff
-        // any #vpon/#vpoff chat received
-        // first T5
+        if (text.match(/^-+ Game Setup -+$/)) {
+            // Initialize
+            gs.vp.humancount = 0;
+            gs.vp.pnamesVPON = [];
+            gs.vp.pnames = [];
+            gs.vp.vpon = false;
+            gs.vp.lock = false;
 
-        // onUpdate():
-        // s = s !== null ? s
-        //   : advbotGame ? lastRequest
-        //   : title has #vpoff ? false
-        //   : title has #vpon ? true
-        //   : anyone said #vpoff ? false
-        //   : null
-        //
-        // c = s !== null ? s : anyone said #vpon
+            // Handle table name #vpon/#vpoff commands
+            var tableName = JSON.parse(dc.table.get('settings')).name;
+            if (tableName.match(/#vpon/i)) {
+                gs.vp.vpon = true;
+                gs.vp.lock = true;
+                announceLock(true);
+            } else if (tableName.match(/#vpoff/i)) {
+                gs.vp.vpon = false;
+                gs.vp.lock = true;
+                announceLock(true);
+            }
 
-        // On my T1:
-        //   if advbotGame && vp_bot_enable:
-        //     send #vpon
-        //   else if s === null:
-        //     if vp_human_request:
-        //       send #vpon
-        //     else if vp_human_disallow:
-        //       send #vpoff
-        // On any T5:
-        //   if s === null
-        //     s = c
+        } else if (text.match(/(.*) - starting cards/)) {
+            // Collect the player names
+            if (!text.match(/(Bottington.*| Bot|Bot [VI]+) - starting cards/)) {
+                gs.vp.humanCount += 1;
+            }
 
-        // Handle bot/adventure games and #vpon/off in title on first message
-        if (messageName === 'gameSetup') {
-            console.log(1);
-            gs.vp = { s: null, p: [], humanCount: 0};
+        } else if (isMyT1(text) && !gs.vp.locked) {
+            if (gs.getOption('vp_request')) {
+                sendChat('#vpon');
+            }
 
-            if (botadvGame) {
-                console.log(2);
-                gs.vp.s = gs.get_option('vp_bot_enable') || null;
+        } else if (text.match(/: turn 5/)) {
+            // Lock counter on the first player's T5
+            gs.vp.vpon = gs.vp.vpon || false;
+            if (!gs.vp.lock && gs.vp.announced) {
+                announceLock(false);
+            }
+            gs.vp.lock = true;
+        }
+    };
+
+    handleChat = function (speaker, text) {
+        console.log('Chat from ' + speaker + ': ' + text);
+        if (text.match(/#vpon/i)) {
+
+            if (gs.vp.locked) {
+                // Do nothing if already locked
+                sendChat('Sorry, my VP counter is locked.');
+
+            } else if (gs.vp.pnamesVPON.indexOf(speaker) === -1) {
+                // Lock ON if all human players have requested #vpon
+                gs.vp.pnamesVPON.push(speaker);
+                gs.vp.vpon = true;
+                if (gs.vp.pnamesVPON.length === gs.vp.humanCount) {
+                    gs.vp.locked = true;
+                    announceLock(false);
+                }
+
+            } else if (gs.getOption('vp_disallow')) {
+                // Automatically refuse if using vp_disallow option
+                sendChat('#vpoff');
+
             } else {
-                console.log(3);
-                gs.vp.s = tablename.match(/#vpoff/i) ? false
-                        : (tablename.match(/#vpon/i) ? true : null);
-            }
-
-        } else if (messageName === 'addLog') {
-            // Get players from the "starting cards" log entries
-            console.log(4);
-
-            m = messageData.text.match(/^(.*) - starting cards/);
-            if (m) {
-                console.log(5);
-                if (!(m[1].match(/^Lord Bottington$/) || m[1].match(/^.* Bot$/))) {
-                    console.log(6);
-                    gs.vp.humanCount += 1;
+                // Otherwise enable
+                gs.vp.vpon = true;
+                // If it's my own #vpon chat, explain it
+                if (speaker === mroom.localPlayer.get('playerName')) {
+                    sendChat('I would like to use a VP Counter (' + salvagerURL + '). '
+                           + 'Say "#vpoff" before turn 5 to disallow it or '
+                           + '"#vp?" any time to see the score in chat.');
                 }
             }
-        }
+         
+        } else if (text.match(/#vpoff/i)) {
+            gs.vp.vpon = false;
+            gs.vp.lock = true;
+            sendChat('My VP counter is now disabled.');
 
-        if (gs.vp.s === null) {
-            console.log(7);
-
-            // Handle #vpon and #vpoff messages
-            if (messageName === 'RoomChat') {
-                console.log(8);
-
-                // Any player can unilaterally disable and lock VP counter
-                if (messageData.text.match(/#vpoff/i)) {
-                    console.log(9);
-                    gs.vp.s = false;
-
-                } else if (messageData.text.match(/#vpon/i)) {
-                    console.log(10);
-                    var speaker = window.mtgRoom.playerList.findByAddress(
-                        messageData.playerAddress
-                    ).get('playerName');
-                    if (gs.vp.p.indexof(speaker) < 0) {
-                        gs.vp.p.push(speaker);
-                    }
-                }
-
-            } else if (messageName === 'addLog') {
-                console.log(11);
-
-                // Handle bot games and auto-VP options on player's T1
-                m = messageData.text.match('-* (.*): turn 1');
-                if (m) {
-                    console.log(12);
-                    console.log('Turn 1');
-                    console.log(m);
-                }
-                if (m && m[1] === window.mtgRoom.localPlayer.get('playerName')) {
-                    console.log(13);
-                    if (gs.vp.humanCount === 1 && gs.get_option('vp_bot_enable')) {
-                        console.log(14);
-                        gs.vp.s = true;
-                    } else {
-                        console.log(15);
-                        if (gs.get_option('vp_human_request') && !botadvGame) {
-                            console.log(16);
-                            dc.clientConnection.send('sendChat', {text: '#vpon'});
-                        } else if (gs.get_option('vp_human_disallow') && !botadvGame) {
-                            console.log(17);
-                            gs.vp.s = false;
-                            dc.clientConnection.send('sendChat', {text: '#vpoff'});
-                        }
-                    }
-                }
-
-                // Lock on anyone's T5
-                m = messageData.text.match('.*: turn 5');
-                if (m) {
-                    console.log(18);
-                    console.log('Turn 5');
-                    gs.vp.s = gs.vp.p.length > 0;
-                }
+        } else if (text.match(/#vp?/i)) {
+            if (gs.vp.vpon) {
+                sendChat(formatForChat(getScores()));
+            } else {
+                sendChat('Sorry, my VP counter is disabled.');
             }
         }
     };
 
-    // Hijack server messages to handle VP counter toggling
-    dc.prototype.onIncomingMessage_orig = dc.prototype.onIncomingMessage;
-    dc.prototype.onIncomingMessage = function (messageName, messageData, message) {
-        try {
-            updateVPCounterToggle(messageName, messageData, message, this);
-        } catch (e) {
-            console.log('Error while updating VP counter toggle');
-            console.log(e.message);
-        } finally {
-            // Process server message like normal
-            this.onIncomingMessage_orig.apply(this, arguments);
-        }
+    announceLock = function (includeURL) {
+        var msg = 'My VP counter is now ' + (gs.vp.vpon ? 'on' : 'disabled') + ' and locked.';
+        if (includeURL) { msg += ' See ' + salvagerURL; }
+        sendChat(msg);
     };
 };
