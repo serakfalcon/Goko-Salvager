1d8a7bb7b896988ac775cded14ec30f49e67e992
Andrew Iannaccone <andrewiemail@gmail.com>
2013-09-05 12:06:39 +0000

    Implement autokick ranges; no chime on failed blacklisted player join

--- src/ext/autokick.js
+++ src/ext/autokick.js
@@ -1,70 +1,115 @@
 /*jslint browser: true, devel: true, indent: 4, vars: true, nomen: true, regexp: true, forin: true, white:true */
-/*global $, _ */
+/*global $, _, Audio */
 
 /*
  * Auto kick module
- *
- * Goko dependencies:
- *   - getRating API specifics ($elPro and $elQuit trigger getting the pro ranking)
- *   - onPlayerJoinTable API
- *   - lot of other APIs (bootTable, table settings, isLocalOwner)
- * Internal dependencies
- *   - option: autokick
- *   - option: blacklist
  */
 var loadAutokickModule = function (gs, zch) {
     "use strict";
 
-    var joinSound = document.createElement('div');
-    joinSound.innerHTML = '<audio id="_joinSound" style="display: none;" src="sounds/startTurn.ogg"></audio>';
-    document.getElementById('viewport').appendChild(joinSound);
-    zch.prototype.old_onPlayerJoinTable = zch.prototype.onPlayerJoinTable;
-    zch.prototype.onPlayerJoinTable = function (t, tp) {
-        this.old_onPlayerJoinTable(t, tp);
-        var p = tp.get('player');
-        var isBot = p.get('isBot');
-
-        if (gs.get_option('autokick') && this.isLocalOwner(t) && !isBot) {
-            var settings = JSON.parse(t.get("settings"));
-            var pro = settings.ratingType === 'pro';
-            var m = settings.name.toLowerCase().match(/\b(\d+)(\d{3}|k)\+/);
-            var mr = null;
-            if (m) {
-                mr = parseInt(m[1], 10) * 1000 + (m[2] === 'k' ? 0 : parseInt(m[2], 10));
-            }
-            var ratingHelper = this.meetingRoom.getHelper('RatingHelper');
-            var self = this;
-            if (mr) {
-                ratingHelper.getRating({
-                    playerId: p.get("playerId"),
-                    $elPro: $(document.createElement('div')),
-                    $elQuit: $(document.createElement('div'))
-                }, function (resp) {
-                    if (!resp.data) {
-                        return;
-                    }
-                    var r = pro ? resp.data.ratingPro : resp.data.rank;
-                    if (r !== undefined && r < mr) {
-                        self.meetingRoom.conn.bootTable({
-                            table: t.get('number'),
-                            playerAddress: p.get('playerAddress')
-                        });
-                    } else {
-                        document.getElementById('_joinSound').play();
-                    }
-                });
-            } else {
-                document.getElementById('_joinSound').play();
-            }
-        }
-
-        if (gs.get_option('blacklist').indexOf(tp.getName()) > -1 && this.isLocalOwner(t)) {
-            this.meetingRoom.conn.bootTable({
-                table: t.get('number'),
-                playerAddress: p.get('playerAddress')
-            });
+    // Parse numbers like 303 and 4.23k
+    var parseNum = function (str) {
+        try {
+            var m = str.match(/^([0-9.]+)([kK]?)$/);
+            return Math.floor(parseFloat(m[1]) * (m[2] !== '' ? 1000 : 1));
+        } catch (e) {
+            // Fail silently if unparseable strings get here
+            return null;
         }
     };
+
+    // Parse titles like X+, Y-, X-Y, and +/-Z
+    // Precedence: +/- > range > min thresh > max thresh
+    var parseRange = function (tablename, myRating) {
+        var m, minRating = null, maxRating = null;
+
+        if ((m = tablename.match(/(\d+(.\d+)?([kK])?)-/)) !== null) { 
+            minRating = null;
+            maxRating = parseNum(m[1]);
+        }
+        if ((m = tablename.match(/(\d+(.\d+)?([kK])?)\+/)) !== null) {
+            minRating = parseNum(m[1]);
+            maxRating = null;
+        }
+        if ((m = tablename.match(/(\d+(.\d+)?([kK])?)-(\d+(.\d+)?([kK])?)/)) !== null) {
+            minRating = parseNum(m[1]);
+            maxRating = parseNum(m[4]);
+        }
+        if ((m = tablename.match(/\+\/-(\d+(.\d+)?([kK])?)/)) !== null) {
+            minRating = myRating - parseNum(m[1]);
+            maxRating = myRating + parseNum(m[1]);
+        }
+
+        return [minRating, maxRating];
+    };
+
+    var getProRating = function (gokoconn, playerId, callback) {
+        gokoconn.getRating({
+            playerId: playerId,
+            ratingSystemId: '501726b67af16c2af2fc9c54'
+        }, function (resp) {
+            return callback(resp.data.rating);
+        });
+    };
+
+    var kickOrNotify = function (gokoconn, table, joiner) {
+
+        // Asynchronously get my rating
+        getProRating(gokoconn, gokoconn.connInfo.playerId, function (myRating) {
+
+            console.log('My rating: ' + myRating);
+
+            // Determine my acceptable rating range
+            var tablename = JSON.parse(table.get("settings")).name;
+            var range = parseRange(tablename, myRating);
+            var minRating = range[0];
+            var maxRating = range[1];
+
+            console.log('My range: ' + range);
+
+            // Asynchronously get joiner's rating
+            getProRating(gokoconn, joiner.get('playerId'), function (hisRating) {
+                console.log('Joiner rating: ' + hisRating);
+
+                var shouldKick = false;
+
+                if (gs.get_option('autokick')
+                    && !joiner.get('isBot') 
+                    && typeof myRating !== 'undefined'
+                    && (typeof hisRating === 'undefined' 
+                        || (minRating !== null && hisRating < minRating)
+                        || (maxRating !== null && hisRating > maxRating))) {
+
+                    // Kick if joiner is rated too high or too low
+                    console.log('Outside my range... kicking');
+                    shouldKick = true;
+
+                } else if (gs.get_option('blacklist')
+                             .indexOf(joiner.get('playerName')) > -1) {
+
+                    // Kick if joiner is on my blacklist
+                    console.log('On my blacklist... kicking');
+                    shouldKick = true;
+                }
+
+                // Kick joiner or notify me with a sound
+                if (shouldKick) {
+                    gokoconn.bootTable({
+                        table: table.get('number'),
+                        playerAddress: joiner.get('playerAddress')
+                    });
+                } else {
+                    new Audio('sounds/startTurn.ogg').play();
+                }
+            });
+        });
+    };
+
+    gs.alsoDo(zch, 'onPlayerJoinTable', null, function (t, tp) {
+        if (this.isLocalOwner(t)) {
+            kickOrNotify(this.meetingRoom.conn, t, tp.get('player'));
+        }
+    });
 };
 
 window.GokoSalvager.depWait(
