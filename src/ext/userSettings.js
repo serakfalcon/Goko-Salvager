d20530acb3097e8f115592c4e00ded0f57a1ead8
Andrew Iannaccone <andrewiemail@gmail.com>
2013-09-09 06:08:43 +0000

    Use depWait() for dependency checking

--- src/ext/userSettings.js
+++ src/ext/userSettings.js
@@ -1,105 +1,79 @@
-/*jslint browser: true, devel: true, indent: 4, vars: true, nomen: true, regexp: true, forin: true */
+/*jslint browser: true, devel: true, indent: 4, vars: true, nomen: true, regexp: true, forin: true, white:true */
 /*global $, _, angular */
 
-var loadUserSettingsModule;
 (function () {
     "use strict";
-
-    console.log('Preparing to load User Settings module');
-
-    // Wait (non-blocking) until the required objects have been instantiated
-    var waitLoop = setInterval(function () {
-        var gs, gso, ang;
-
-        console.log('Checking for User Settings dependencies');
-
-        try {
-            gs = window.GokoSalvager;
-            gso = gs.get_option;
-        } catch (e) {}
-
-        if ($('.fs-rs-logout-row').length > 0
-                && typeof gso !== 'undefined' && gso !== null
-                && typeof window.angular !== 'undefined'
-                && typeof $('div').dialog !== 'undefined') {
-            console.log('Loading User Settings module');
-            loadUserSettingsModule(gs);
-            clearInterval(waitLoop);
-        }
-    }, 100);
-}());
-
-/*
- * GokoSalvager UserSettings Module: displays option menu on login screen
- *
- * Goko dependencies:
- *   - FS.Templates.LaunchScreen
- *   - '.fs-rs-logout-row' HTML widget
- * Internal dependencies:
- *   - GokoSalvager option methods
- */
-loadUserSettingsModule = function (gs) {
-    "use strict";
-
-    window.gokoSalvagerUserSettingsController = function ($scope) {
-        // Load settings on initialization
-        $scope.us = gs.get_options();
-        gs.debug($scope.us);
-
-        $scope.add_to_blacklist = function (pname) {
-            if (pname) {
-                $scope.us.blacklist.push(pname);
-            }
-            $scope.temp_bl = null;
-            $scope.save($scope.us);
-        };
-
-        $scope.rem_from_blacklist = function (pname) {
-            gs.debug('adding ' + pname + ' to blacklist.');
-            if (pname) {
-                $scope.us.blacklist =
-                    $scope.us.blacklist.filter(function (p) {
-                        return p !== pname;
-                    });
-            }
-            $scope.save($scope.us);
-        };
-
-        $scope.saveVPReq = function (us) {
-            us.vp_refuse = us.vp_refuse && !us.vp_request;
-        };
-
-        $scope.saveVPRef = function (us) {
-            us.vp_request = us.vp_request && !us.vp_refuse;
-        };
-
-        $scope.save = function (us) {
-            gs.debug(angular.copy(us));
-            gs.set_options(angular.copy(us));
-        };
-    };
-
-    // Open dialog with the "User Settings" link
-    $('.fs-rs-logout-row').append(
-        $('<div></div>')
-            .addClass('fs-lg-settings-btn')
-            .html('User Settings')
-            .click(function () {
-                // Initialize if necessary
-                if ($('#usDialog').length === 0) {
-                    $('#viewport').append(gs.createSettingsDialog());
-                    angular.bootstrap($('#usDialog'));
+    
+    /*
+     * GokoSalvager UserSettings Module: displays option menu on login screen
+     */
+    var loadUserSettings = function (gs) {
+    
+        window.gokoSalvagerUserSettingsController = function ($scope) {
+            // Load settings on initialization
+            $scope.us = gs.get_options();
+            gs.debug($scope.us);
+    
+            $scope.add_to_blacklist = function (pname) {
+                if (pname) {
+                    $scope.us.blacklist.push(pname);
                 }
-                // Display as JQueryUI popup dialog
-                $('#usDialog').dialog({
-                    modal: true,
-                    width: 550,
-                    closeText: 'Save',
-                    draggable: false,
-                    resizeable: false,
-                    autoOpen: false
-                });
-                $('#usDialog').dialog('open');
-            })
+                $scope.temp_bl = null;
+                $scope.save($scope.us);
+            };
+    
+            $scope.rem_from_blacklist = function (pname) {
+                gs.debug('adding ' + pname + ' to blacklist.');
+                if (pname) {
+                    $scope.us.blacklist =
+                        $scope.us.blacklist.filter(function (p) {
+                            return p !== pname;
+                        });
+                }
+                $scope.save($scope.us);
+            };
+    
+            $scope.saveVPReq = function (us) {
+                us.vp_refuse = us.vp_refuse && !us.vp_request;
+            };
+    
+            $scope.saveVPRef = function (us) {
+                us.vp_request = us.vp_request && !us.vp_refuse;
+            };
+    
+            $scope.save = function (us) {
+                gs.debug(angular.copy(us));
+                gs.set_options(angular.copy(us));
+            };
+        };
+    
+        // Open dialog with the "User Settings" link
+        $('.fs-rs-logout-row').append(
+            $('<div></div>')
+                .addClass('fs-lg-settings-btn')
+                .html('User Settings')
+                .click(function () {
+                    // Initialize if necessary
+                    if ($('#usDialog').length === 0) {
+                        $('#viewport').append(gs.createSettingsDialog());
+                        angular.bootstrap($('#usDialog'));
+                    }
+                    // Display as JQueryUI popup dialog
+                    $('#usDialog').dialog({
+                        modal: true,
+                        width: 550,
+                        closeText: 'Save',
+                        draggable: false,
+                        resizeable: false,
+                        autoOpen: false
+                    });
+                    $('#usDialog').dialog('open');
+                })
+        );
+    };
+    
+    window.GokoSalvager.depWait(
+        ['GokoSalvager', 'jQuery', 'angular', '.fs-rs-logout-row'],
+        100, loadUserSettings, this, 'User Settings'
     );
-};
+}());
