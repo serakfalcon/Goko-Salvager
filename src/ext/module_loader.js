df9225ad0b69635addb2385b34a3d0da584cc29c
Andrew Iannaccone <andrewiemail@gmail.com>
2013-09-28 12:50:51 +0000

    Add looping to ensure all modules load eventually

--- src/ext/module_loader.js
+++ src/ext/module_loader.js
@@ -4,32 +4,30 @@
 (function () {
     "use strict";
 
-    // List of GS modules still waiting for their dependencies
-    var toLoad = Object.keys(GS.modules).map(function(key){
-        return GS.modules[key];
-    });
-
     // How many times each module has failed to find its dependencies
     var failCounts = {};
-    toLoad.map(function (mod) {
-        failCounts[mod.name] = 0;
-    });
 
-    var notLoaded = function (mod) {
-        return !mod.loaded;
-    };
+    // Track which modules have been loaded
+    var loaded = [];
 
-    // Try to load all not-yet-loaded modules
+    // Try to load all not-yet-loaded modules.
+    // Keep looping indefinitely in case this runs before all modules
+    // have been defined.
     var intvl = setInterval(function () {
-        toLoad = toLoad.filter(notLoaded);
+        // List of GS modules still waiting for their dependencies
+        var toLoad = Object.keys(GS.modules).map(function(key){
+            return GS.modules[key];
+        }).filter(function (mod) {
+            return loaded.indexOf(mod.name) === -1;
+        });
 
         var i, mod, missing;
         for (i = 0; i < toLoad.length; i += 1) {
             mod = toLoad[i];
             missing = mod.getMissingDeps();
             if (missing.length === 0) {
-                mod.loaded = true;
                 mod.load();
+                loaded.push(mod.name);
             } else {
                 failCounts[mod.name] += 1;
                 if (failCounts[mod.name] % 10 === 0) {
