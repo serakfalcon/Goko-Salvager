06d0577b894245ab75d2f2fbecb5250af00977f9
Andrew Iannaccone <andrewiemail@gmail.com>
2014-03-09 03:52:40 -0700

    Fix typo

--- src/ext/connection.js
+++ src/ext/connection.js
@@ -68,7 +68,7 @@
                 switch (d.msgtype) {
                 case 'REQUEST_CLIENT_INFO':
                     var info = {
-                        username: mtgRoom.conn.connInfo.playername,
+                        playerName: mtgRoom.conn.connInfo.playerName,
                         playerId: mtgRoom.conn.connInfo.playerId,
                         gsversion: GS.version
                     };
