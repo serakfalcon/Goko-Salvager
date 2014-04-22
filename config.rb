d2a7e886273cd011e00ce113b14f76b0aa99a0fa
Andrew Iannaccone <andrewiemail@gmail.com>
2014-04-22 02:03:24 -0700

    Increment version number for beta release.
    I think this version is ready to become the official release.  I don't
    know for sure that it's bug-free, but it's definitely better than the
    current official version.

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 {
     # Info for extension configuration files (package.json, manifest.json, etc)
     :name => 'gokosalvager',
-    :version => '2.5.4.0',
+    :version => '2.5.4.1',
     :title => 'Goko Dominion Salvager',
     :desc => 'Enhance your Online Dominion experience',
     :author => 'The unofficial forum.dominionstrategy dev team',
